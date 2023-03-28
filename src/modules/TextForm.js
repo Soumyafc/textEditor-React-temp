bf6b60f2f7db099d86fc10f259bfc4e6543880d9
akashmishra98 <83551583+akashmishra98@users.noreply.github.com>
2023-03-29 01:34:27 +0530

    fixed : navbar ,addition : added count function and preview function

--- src/modules/TextForm.js
+++ src/modules/TextForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {createElement, useState} from 'react';
 
 export default function TextForm(props) {
     
@@ -7,19 +7,41 @@ export default function TextForm(props) {
          let newText = text.toLocaleUpperCase();
          setText(newText);
     }
+    const handlelowClick = ()=>{
+        let newText = text.toLocaleLowerCase();
+        setText(newText);
+    }
+    // const handleitalicClick = ()=>{
+    //     let newText = text;
+    //     setText(newText);
+    // }
+    
     const handleOnChange = (event)=>{
         // console.log("on change");
         setText(event.target.value);
      }
-    const [text, setText] = useState('enter text here');
+    const [text, setText] = useState('');
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                {/* <label htmlFor = "myBox" className="form-label">Example of text</label> */}
-                <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" cols="30" rows="10"></textarea>
+        <>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    {/* <label htmlFor = "myBox" className="form-label">Example of text</label> */}
+                    <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" cols="30" rows="10"></textarea>
+                </div>
+                <button className="btn btn-primary" onClick={handleUpClick}>Covert to uppercase</button>
+                &nbsp;
+                <button className="btn btn-primary" onClick={handlelowClick}>Covert to lowercase</button>
+                &nbsp;
+                {/* <button className="btn btn-primary" onClick={handleitalicClick}>Covert to italics</button> */}
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Covert to uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h3>Your Text Summary</h3>
+                <p> <b>{text.split(" ").length}</b>,<b> {text.length} </b>characters</p>
+                <p>Avarage time to read is {0.008*text.length} Minutes</p>
+                <h3>Preview of text</h3>
+                <p> {text} </p>
+            </div>
+        </>
     )
 }
